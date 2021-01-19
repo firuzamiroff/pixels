8903c22d44c2651462b5ee505e78f0ff3cb84906
Firuz Amirov <59014178+firuzamiroff@users.noreply.github.com>
2021-01-19 23:24:38 +0400

    Make More Change
    Add Avatars
    Delete Back fill
    Add Arrows
    Add Input Enter
    Add Input Username

--- js/script.js
+++ js/script.js
@@ -3,3 +3,24 @@ $(document).ready(function() {
 		$(".logo-wrapper").toggleClass("on")
 	});
 });
+
+var avatars = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var avatar = 0
+$(".left").click(function() {
+	console.log("Left")
+	avatar = avatar - 1
+	console.log(avatar)
+	console.log(avatars[avatar])
+});
+$(".right").click(function() {
+	console.log("Right")
+	avatar = avatar + 1
+	console.log(avatar)
+	console.log(avatars[avatar])
+});
+$(".up").click(function() {
+	console.log("Up")
+});
+$(".down").click(function() {
+	console.log("Down")
+});
