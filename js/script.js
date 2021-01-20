dd929e58693838e283701221376f89c6bbdf649a
Firuz Amirov <59014178+firuzamiroff@users.noreply.github.com>
2021-01-20 12:42:28 +0400

    Final arrows Js
    Add style for arrows in Js
    Fix and add func for arrows

--- js/script.js
+++ js/script.js
@@ -1,52 +1,78 @@
+var avatars = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var avatarcolors = ["black", "#ab1d19", "#7ec5f9", "#cf629b", "#59a259", "#9334ad", "#fda500", "#797130"];
+var avatar = randomNumber(0, avatars.length);
+var avatarcolor = randomNumber(0, avatarcolors.length);
+var user = {
+	name: "",
+	lang: "",
+	avatar: "",
+	avatarcolor: ""
+};
+
+function randomNumber(min, max){
+    const r = Math.random()*(max-min) + min
+    return Math.floor(r)
+}
+
+$(".left").click(function() {
+	console.log("left")
+	avatar--;
+	if (avatar< 0) {
+		avatar = avatars.length-1;
+		$(".avatar").attr("src", "img/avatar"+avatars[avatar]+".svg");
+	} else {
+		$(".avatar").attr("src", "img/avatar"+avatars[avatar]+".svg");
+	}
+});
+$(".right").click(function() {
+	console.log("right")
+	avatar++;
+	if (avatar>avatars.length-1) {
+		avatar -= avatars.length;
+		$(".avatar").attr("src", "img/avatar"+avatars[avatar]+".svg");
+	} else {
+		$(".avatar").attr("src", "img/avatar"+avatars[avatar]+".svg");
+	}
+});
+$(".up").click(function() {
+	console.log("up")
+	avatarcolor--;
+	if (avatarcolor < 0) {
+		avatarcolor = avatarcolors.length-1;
+		$(".avatar").css("background-color", avatarcolors[avatarcolor]);
+		$( ".avatar" ).css("box-shadow", " 0 0 0 5px " + avatarcolors[avatarcolor]);
+	} else if (avatarcolor === 0) {
+		$(".avatar").css("background-color", avatarcolors[avatarcolor]);
+		$( ".avatar" ).css("box-shadow", " 0 0 0 5px " + "#333");
+	} else {
+		$(".avatar").css("background-color", avatarcolors[avatarcolor]);
+		$( ".avatar" ).css("box-shadow", " 0 0 0 5px " + avatarcolors[avatarcolor]);
+	}
+});
+$(".down").click(function() {
+	console.log("down")
+	avatarcolor++;
+	if (avatarcolor > avatarcolors.length-1) {
+		avatarcolor = avatarcolor - avatarcolors.length;
+		$(".avatar").css("background-color", avatarcolors[avatarcolor]);
+		$( ".avatar" ).css("box-shadow", " 0 0 0 5px " + "#333");
+	} else {
+		$(".avatar").css("background-color", avatarcolors[avatarcolor]);
+		$( ".avatar" ).css("box-shadow", " 0 0 0 5px " + avatarcolors[avatarcolor]);
+	}
+});
+
 $(document).ready(function() {
 	$(".logo-wrapper").click(function(){
 		$(".logo-wrapper").toggleClass("on")
 	});
+	$( ".avatar" ).attr("src", "img/avatar"+avatars[avatar]+".svg");
+	$( ".avatar" ).css("background-color", avatarcolors[avatarcolor]);
+
+	if (avatarcolor === 0) {
+		$( ".avatar" ).css("box-shadow", " 0 0 0 5px " + "#333");
+	} else {
+		$( ".avatar" ).css("box-shadow", " 0 0 0 5px " + avatarcolors[avatarcolor]);
+	}
 });
 
-var avatars = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var avatarcolors = ["black", "red", "green", "blue", "ping"]
-var avatar = 0
-var avatarlink = ""
-var avatarcolor = 0
-
-$(".left").click(function() {
-	avatar = avatar - 1;
-	if (avatar<0) {
-		avatar = 0;
-		avatarlink = "img/avatar"+avatars[avatar]+".svg"
-		$( ".avatar" ).attr("src",avatarlink);
-	} else {
-		avatarlink = "img/avatar"+avatars[avatar]+".svg"
-		$( ".avatar" ).attr("src",avatarlink);
-	}
-});
-$(".right").click(function() {
-	avatar = avatar + 1;
-	if (avatar>8) {
-		avatar = 8;
-		avatarlink = "img/avatar"+avatars[avatar]+".svg"
-		$( ".avatar" ).attr("src",avatarlink);
-	} else {
-		avatarlink = "img/avatar"+avatars[avatar]+".svg"
-		$( ".avatar" ).attr("src",avatarlink);
-	}
-});
-$(".up").click(function() {
-	avatarcolor = avatarcolor - 1;
-	if (avatarcolor<0) {
-		avatarcolor = 0;
-		$( ".avatar" ).css("background-color", avatarcolors[avatarcolor]);
-	} else {
-		$( ".avatar" ).css("background-color", avatarcolors[avatarcolor]);
-	}
-});
-$(".down").click(function() {
-	avatarcolor = avatarcolor + 1;
-	if (avatarcolor>4) {
-		avatarcolor = 4;
-		$( ".avatar" ).css("background-color", avatarcolors[avatarcolor]);
-	} else {
-		$( ".avatar" ).css("background-color", avatarcolors[avatarcolor]);
-	}
-});
