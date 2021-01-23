b9f67d2f7e936d7c23ce66a532a1a1a89b6e6486
Firuz Amirov <59014178+firuzamiroff@users.noreply.github.com>
2021-01-23 12:54:46 +0400

    Update second page
    HTML CSS JS

--- lounge/script.js
+++ lounge/script.js
@@ -1,13 +1,6 @@
 $(document).ready(function() {
-	//const myData = JSON.parse(localStorage.getItem('User'));
-	//document.getElementById('lololo').innerHTML = myData["username"];
-	//document.getElementById("avatar").src = "../img/avatar"+ myData["avatar"] +".svg";
-	//document.getElementById("avatar").style = "background-color: "+ myData["avatarcolor"] +";";
-	
-	$(".logo-wrapper").click(function(){$(".logo-wrapper").toggleClass("on")});
-
-	/*const myData = JSON.parse(localStorage.getItem('User'));
-	$(".lololo").innerHTML = myData["username"];
+	const myData = JSON.parse(localStorage.getItem('User'));
 	$(".avatar").attr("src", "../img/avatar"+ myData["avatar"] +".svg");
-	$(".avatar").css("background-color: "+ myData["avatarcolor"] +";");*/
+	$(".avatar").css("background-color", myData["avatarcolor"]);
+	$(".name").text(myData["username"]);
 });
