49943ab5c1c6efcd3a95f72c4ec78e1b717126c0
Firuz Amirov <59014178+firuzamiroff@users.noreply.github.com>
2021-01-30 22:19:11 +0400

    Update

--- lounge/script.js
+++ lounge/script.js
@@ -4,3 +4,11 @@ $(document).ready(function() {
 	$(".avatar").css("background-color", myData["avatarcolor"]);
 	$(".name").text(myData["username"]);
 });
+
+$(".create-room").click(function() {
+	document.location.href = "../createroom/index.html";
+});
+
+$(".room-name").click(function() {
+	document.location.href = "../chat/index.html";
+});
